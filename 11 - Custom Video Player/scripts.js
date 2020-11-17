8b469cce8e3e0307b39916f1153723ef92d32f4f
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-17 18:26:57 -0500

    add scrub

--- 11 - Custom Video Player/scripts.js
+++ 11 - Custom Video Player/scripts.js
@@ -22,11 +22,33 @@ function skip() {
   video.currentTime += parseFloat(this.dataset.skip);
 }
 
+function handleRangeUpdate() {
+  video[this.name] = this.value;
+}
+
+function handleProgress() {
+  const percent = video.currentTime / video.duration * 100;
+  progressBar.style.flexBasis = `${percent}%`;
+}
+
+function scrub(e) {
+  const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+  video.currentTime = scrubTime;
+}
+
 // Event Listeners
 video.addEventListener("click", togglePlay);
 video.addEventListener("play", updateButton);
 video.addEventListener("pause", updateButton);
+video.addEventListener("timeupdate", handleProgress);
 
 toggle.addEventListener("click", togglePlay);
 
 skipButtons.forEach(button => button.addEventListener("click", skip));
+ranges.forEach(range => range.addEventListener("change", handleRangeUpdate));
+
+progress.addEventListener("click", scrub);
+let mouseDown = false;
+progress.addEventListener("mousedown", () => mouseDown = true);
+progress.addEventListener("mouseup", () => mouseDown = false);
+progress.addEventListener("mousemove", (e) => mouseDown && scrub(e));
