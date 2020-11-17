5904810b4ba066be8468d443cb574b08a4c6c0c2
stevenjmarsh <stevenjmarsh@users.noreply.github.com>
2020-11-17 16:10:45 -0500

    add togglePlay

---
+++ 11 - Custom Video Player/scripts.js
@@ -0,0 +1,16 @@
+// Select / Define Elements
+const player = document.querySelector(".player");
+const video = player.querySelector(".viewer");
+const progress = player.querySelector(".progress");
+const progressBar = player.querySelector(".progress__filled");
+const toggle = player.querySelector(".toggle");
+const skipButtons = player.querySelectorAll("[data-skip]");
+const ranges = player.querySelectorAll(".player__slider");
+
+// Functions
+const togglePlay = () => {
+  return video.paused ? video.play() : video.pause();
+}
+
+// Event Listeners
+video.addEventListener("click", togglePlay);
